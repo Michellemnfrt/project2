bc77ccf79a50e5628c53f65b5f9e9a48fb8f3e8f
Michelle Monfort <Michellemnfrt@gmail.com>
2018-06-14 17:05:34 -0400

    no crashskiepoopws

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const hbs = require("hbs");
 const app = express();
 const bodyParser =require('body-parser');
-const router = require('./routes/app.js');
+const router = require('./routes/index.js');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "hbs");
 
 
-app.use(require("./routes/index.js"));
+app.use(router);
 
 app.set('port', process.env.PORT || 3000) 
 app.listen(3000, () => console.log('app is running in server'));
